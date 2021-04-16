e3d81048786cd17a275f05ba380d835400f618ca
programacion <estar@minuevo>
2021-04-16 09:28:45 +0200

    antes de las pruebas de colocarminas

---
+++ src/modelo/CoordenadaTest.java
@@ -0,0 +1,19 @@
+package modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CoordenadaTest {
+
+	@Test
+	void testEqualsObject() {
+		//porque hay que probar!! asgurarse de que se ha sobreescrito el equals
+		Coordenada coordenada = new Coordenada(0, 0);
+		Coordenada coordenada2 = new Coordenada(0, 0);
+		Coordenada coordenada3 = new Coordenada(3, 3);
+		assertTrue(coordenada.equals(coordenada2));
+		assertFalse(coordenada.equals(coordenada3));
+	}
+
+}
