de886f03b1ac1b4ae1af67425df20ea275057389
programacion <estar@minuevo>
2021-04-19 17:29:45 +0200

    tablero aleatorio

---
+++ src/modelo/TableroAleatorio.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class TableroAleatorio extends Tablero {
+
+	public TableroAleatorio(int lado,int minas) {
+		super(lado);
+		colocarMinasAleatorio(minas);
+	}
+
+	private void colocarMinasAleatorio(int minas) {
+		//TODO 
+	}
+
+}
